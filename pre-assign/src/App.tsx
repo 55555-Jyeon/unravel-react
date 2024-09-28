354e50e7796c80dd6c8516af2ff040576a9e9973
55555-Jyeon <jyeon380516@gmail.com>
2024-09-28 19:16:42 +0900

    create infinite scroll with intersection observer

--- pre-assign/src/App.tsx
+++ pre-assign/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/layout/header";
-import ProductList from "./components/productList";
+import ProductList from "./components/product-list";
 
 function App() {
   return (
--- pre-assign/src/components/product.tsx
+++ pre-assign/src/components/product.tsx
@@ -1,6 +1,6 @@
 import "./product.css";
 
 const Product = () => {
-  return <div className="section">product data</div>;
+  return <div className="card">product data</div>;
 };
 export default Product;
